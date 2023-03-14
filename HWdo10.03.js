faa04988dee587731f63a5c6c2b69fdc4fb4eb67
b1sokol <113973047+b1sokol@users.noreply.github.com>
2023-03-14 19:46:03 +0100

    hwdo10.03(done)

--- HWdo10.03.js
+++ HWdo10.03.js
@@ -1,1 +1,13 @@
-function 
+//1
+const buttButton = document.getElementById("butt");
+buttButton.onclick = () =>
+    alert ("Cogratulation! You made it!)");
+//2
+function addSpanToPage(text) {
+    const span = document.createElement('span');
+    span.innerHTML = text;
+    document.body.appendChild(span);
+  }
+  addSpanToPage('Hello my dear friends!');
+
+  
